76179f783d5e4c70d6949c8f4f717a3e76157542
visti <emilvisti@gmail.com>
2018-08-22 13:08:42 +0200

    Update reddit_OP_comment_checker.py

--- reddit_OP_comment_checker.py
+++ reddit_OP_comment_checker.py
@@ -3,9 +3,9 @@ import datetime
 import time
 
 def main():
-    reddit = praw.Reddit(client_id='T80z6DeG_mJL7A',
-                        client_secret='nYV5dPqc94rPLkRaYjr1QNWDxSo',
-                        password='klonklon',
+    reddit = praw.Reddit(client_id='',
+                        client_secret='',
+                        password='',
                         username='guitar_remover_bot',
                         user_agent='my user agent')
 
